7426e0c3037015c65f9f3ad15da3005e656841a9
Ethan Yu <eykyudev@gmail.com>
2023-06-07 00:39:28 -0700

    Update Header.tsx

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -16,7 +16,7 @@ function Header() {
   return (
     <div className={`header ${fade ? 'fade-in' : ''}`}>
       <span className='header-text'>
-        Hi! My name is Ethan. I'm a passionate software engineer that loves learning new ways to provide <span className="gradient-text-2">optimal solutions</span> to turn your <span className="gradient-text">designs into code</span>, and I would love for the chance to <span className="gradient-text-2">connect and work </span>for you.
+        Hi! My name is Ethan. I'm a passionate software engineer that loves learning new ways to provide <span className="gradient-text">optimal solutions</span> to turn your <span className="gradient-text">designs into code</span>, and I would love for the chance to <span className="gradient-text-2">connect and work </span>for you.
       </span> 
     </div>
   );
