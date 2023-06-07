aa70a20b6d8547b8b68b108f147f536e03dcdfbd
Ethan Yu <eykyudev@gmail.com>
2023-06-07 00:22:38 -0700

    updated hrefs

--- src/components/Cards.tsx
+++ src/components/Cards.tsx
@@ -8,8 +8,8 @@ function Cards() {
         <div className='decks'>
           <div className='card-deck'>
             <Card img='path/to/image.jpg' title='flos-plants' titledate='e-commerce site!' button1='deployment' button2='github' href1='flos-frontend.vercel.app/' href2='github.com/eykyu-dev/flos-frontend'/>
-            <Card img='path/to/image.jpg' title='portfolio' titledate='this site!' button1='deployment' button2='github' href1='flos-frontend.vercel.app/' href2='github.com/eykyu-dev/flos-frontend'/>
-            <Card img='path/to/image.jpg' title='pitcher' titledate='pitch-training app!' button1='deployment' button2='github' href1='flos-frontend.vercel.app/' href2='github.com/eykyu-dev/flos-frontend'/>  
+            <Card img='path/to/image.jpg' title='portfolio' titledate='this site!' button1='deployment' button2='github' href1='portfolio-eykyu-dev.vercel.app/' href2='github.com/eykyu-dev/portfolio'/>
+            <Card img='path/to/image.jpg' title='pitcher' titledate='pitch-training app!' button1='deployment' button2='github' href1='pitcher-inky.vercel.app/' href2='github.com/eykyu-dev/pitcher'/>  
           </div>
         </div>
     </div>
