4e1da56f8b60e1ccc74bf737c115697126612f50
Ethan Yu <eykyudev@gmail.com>
2023-06-08 16:56:57 -0700

    Update CardShowcase.tsx

--- src/components/CardShowcase.tsx
+++ src/components/CardShowcase.tsx
@@ -5,9 +5,9 @@ function CardShowcase() {
   return (
     <div className='showcase-div'>
         <div className='cards-in-showcase-div'>
-            <Card img='public/flos.png' title='flos-plants' titledate='e-commerce site!' button1='deployment 🧑🏻‍💻' button2='github 📂' href1='flos-frontend.vercel.app/' href2='github.com/eykyu-dev/flos-frontend'/>
-            <Card img='public/pitcher.png' title='pitcher' titledate='pitch-training site!' button1='deployment 🧑🏻‍💻' button2='github 📂' href1='flos-frontend.vercel.app/' href2='github.com/eykyu-dev/flos-frontend'/>
-            <Card img='public/pitcher.png' title='cash-wallet' titledate='c++ back-end' button1='deployment 🧑🏻‍💻' button2='github 📂' href1='flos-frontend.vercel.app/' href2='github.com/eykyu-dev/flos-frontend'/>
+            <Card img='/flos.png' title='flos-plants' titledate='e-commerce site!' button1='deployment 🧑🏻‍💻' button2='github 📂' href1='flos-frontend.vercel.app/' href2='github.com/eykyu-dev/flos-frontend'/>
+            <Card img='/pitcher.png' title='pitcher' titledate='pitch-training site!' button1='deployment 🧑🏻‍💻' button2='github 📂' href1='flos-frontend.vercel.app/' href2='github.com/eykyu-dev/flos-frontend'/>
+            <Card img='/pitcher.png' title='cash-wallet' titledate='c++ back-end' button1='deployment 🧑🏻‍💻' button2='github 📂' href1='flos-frontend.vercel.app/' href2='github.com/eykyu-dev/flos-frontend'/>
         </div>
     </div>
   );
