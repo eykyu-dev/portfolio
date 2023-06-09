aba3124aab12c25d279265ebc3429e9f92df203b
Ethan Yu <eykyudev@gmail.com>
2023-06-08 18:20:21 -0700

    added card

--- src/components/CardShowcase.tsx
+++ src/components/CardShowcase.tsx
@@ -7,7 +7,7 @@ function CardShowcase() {
         <div className='cards-in-showcase-div'>
             <Card img='/flos.png' title='flos-plants' titledate='e-commerce site!' button1='deployment 🧑🏻‍💻' button2='github 📂' href1='flos-frontend.vercel.app/' href2='github.com/eykyu-dev/flos-frontend'/>
             <Card img='/pitcher.png' title='pitcher' titledate='pitch-training site!' button1='deployment 🧑🏻‍💻' button2='github 📂' href1='pitcher-inky.vercel.app/' href2='github.com/eykyu-dev/pitcher'/>
-            <Card img='/pitcher.png' title='cash-wallet' titledate='c++ back-end' button1='deployment 🧑🏻‍💻' button2='github 📂' href1='flos-frontend.vercel.app/' href2='github.com/eykyu-dev/flos-frontend'/>
+            <Card img='/model-viewer.png' title='model-viewer' titledate='3d-model-viewer from scratch' button1='deployment 🧑🏻‍💻' button2='github 📂' href1='model-viewer-eykyu-dev.vercel.app/' href2='github.com/eykyu-dev/model-viewer'/>
         </div>
     </div>
   );
