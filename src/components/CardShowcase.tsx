4699143163640e17c0d4a3585b5d87bc56f6c8cc
Ethan Yu <eykyudev@gmail.com>
2023-06-08 17:06:25 -0700

    Update CardShowcase.tsx

--- src/components/CardShowcase.tsx
+++ src/components/CardShowcase.tsx
@@ -6,7 +6,7 @@ function CardShowcase() {
     <div className='showcase-div'>
         <div className='cards-in-showcase-div'>
             <Card img='/flos.png' title='flos-plants' titledate='e-commerce site!' button1='deployment 🧑🏻‍💻' button2='github 📂' href1='flos-frontend.vercel.app/' href2='github.com/eykyu-dev/flos-frontend'/>
-            <Card img='/pitcher.png' title='pitcher' titledate='pitch-training site!' button1='deployment 🧑🏻‍💻' button2='github 📂' href1='flos-frontend.vercel.app/' href2='github.com/eykyu-dev/flos-frontend'/>
+            <Card img='/pitcher.png' title='pitcher' titledate='pitch-training site!' button1='deployment 🧑🏻‍💻' button2='github 📂' href1='pitcher-inky.vercel.app/' href2='github.com/eykyu-dev/pitcher'/>
             <Card img='/pitcher.png' title='cash-wallet' titledate='c++ back-end' button1='deployment 🧑🏻‍💻' button2='github 📂' href1='flos-frontend.vercel.app/' href2='github.com/eykyu-dev/flos-frontend'/>
         </div>
     </div>
